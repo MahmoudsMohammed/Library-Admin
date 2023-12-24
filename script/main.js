2d804cff018ff777d9056b85717ccb5dcf481a3c
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2023-12-24 17:35:50 +0200

    get UI Elements

---
+++ script/main.js
@@ -0,0 +1,9 @@
+// UI Elements
+const container = document.querySelector('.main'),
+  title = document.getElementById('title'),
+  author = document.getElementById('author'),
+  isbn = document.getElementById('isbn'),
+  submit = document.getElementById('submit'),
+  tBody = document.querySelector('table tbody');
+
+// Event Listener
