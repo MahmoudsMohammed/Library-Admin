968afd81ec889535f95f85682f8e83674571b72f
Mahmoud Mohammed Sayed <88338661+MahmoudsMohammed@users.noreply.github.com>
2023-12-24 18:41:16 +0200

    store books in local storage

--- script/main.js
+++ script/main.js
@@ -1,5 +1,6 @@
 // UI Elements
-const header = document.querySelector('h1'),
+const alert = document.querySelector('.alert'),
+  container = document.querySelector('.main'),
   title = document.getElementById('title'),
   author = document.getElementById('author'),
   isbn = document.getElementById('isbn'),
@@ -7,6 +8,8 @@ const header = document.querySelector('h1'),
   tBody = document.querySelector('table tbody');
 
 // Create Classes
+
+// Class For Books
 class Book {
   constructor(t, a, i) {
     this.title = t;
@@ -14,25 +17,47 @@ class Book {
     this.isbn = i;
   }
 }
+// Class Contain Methods handel UI
 class UI {
   static showMessage(msg, check) {
-    let alert = document.createElement('p');
     alert.textContent = msg;
-    alert.className = `alert alert-${check ? 'success' : 'danger'} col-md-8`;
-    header.after(alert);
+    alert.className = `alert d-block alert-${
+      check ? 'success' : 'danger'
+    } col-md-8`;
     setTimeout(() => {
-      alert.remove();
+      alert.className += ' d-none';
     }, 2000);
   }
   static addBook(obj) {
     let row = document.createElement('tr');
-    row.innerHTML = `<td>${obj.title}</td><td>${obj.author}</td><td>${obj.isbn}</td> `;
+    row.innerHTML = `<td>${obj.title}</td><td>${obj.author}</td><td class="d-flex justify-content-between">${obj.isbn} <a href="#" class="text-decoration-none fw-bold text-danger delete">X</a></td> `;
     tBody.append(row);
   }
   static clearInput() {
     title.value = '';
     author.value = '';
     isbn.value = '';
+  }
+  static deleteBook(target) {
+    target.parentElement.parentElement.remove();
+    this.showMessage('Book Removed ;)', true);
+  }
+}
+// Class Deale with local storage
+class Store {
+  static getData() {
+    let books;
+    if (localStorage.books === undefined) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.books);
+    }
+    return books;
+  }
+  static saveBook(book) {
+    let books = this.getData();
+    books.push(book);
+    localStorage.books = JSON.stringify(books);
   }
 }
 // Event Listener
@@ -42,8 +67,15 @@ submit.addEventListener('click', (e) => {
   } else {
     let book = new Book(title.value, author.value, isbn.value);
     UI.addBook(book);
+    Store.saveBook(book);
     UI.clearInput();
     UI.showMessage('Book Added :)', true);
   }
   e.preventDefault();
+});
+
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('delete')) {
+    UI.deleteBook(e.target);
+  }
 });
